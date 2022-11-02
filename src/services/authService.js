b27cef69bbd3204c180c4f9e26a005e3dd544862
Richard Sneyd <richardsneyd@hotmail.com>
2022-11-02 16:35:07 +0000

    adds jwt parsing and local storage

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,39 @@
+import { getSuggestedQuery } from '@testing-library/react';
 import axios from 'axios';
 
-const BASE = "auth/";
 const PORT = 8080;
 
 const AuthenticationService = {
     // {}
-    signin:({username, password})=> {
+    signin: async ({username, password})=> {
         const userParams = this.toUrlEncoded({username, password});
-        fetch(window.location.hostname + ":" + PORT + '/login', {
+        const response = await fetch(window.location.protocol + "//" + window.location.hostname + ":" + PORT + '/login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
             },
             body: userParams
-          })
+          });
+        const json = await response.json();
+          this.storeAccessToken(json.access_token);
+          this.storeUser(this.parseJwt(json.accessToken));
+        return response;
+    },
+
+    storeAccessToken: (accessToken)=> {
+        localStorage.set('access_token', accessToken);
+    },
+
+    getAccessToken: () => {
+        localStorage.getItem('access_token');
+    },
+
+    storeUser: (user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+    },
+
+    getUser: () => {
+        return JSON.parse(localStorage.getItem('user'));
     },
 
     logout: ()=> {
@@ -34,6 +54,16 @@ const AuthenticationService = {
         }
         formBody = formBody.join("&");
         return formBody;
+    },
+
+    parseJwt: (token) => {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    
+        return JSON.parse(jsonPayload);
     }
 }
 
