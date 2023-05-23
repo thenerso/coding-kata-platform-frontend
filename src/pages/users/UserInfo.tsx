aa6538da14710871f2c2604fe4d06ad1a2906f21
Richard Sneyd <richardsneyd@hotmail.com>
2023-05-23 10:08:10 +0100

    removes redundant code

--- src/pages/users/UserInfo.tsx
+++ src/pages/users/UserInfo.tsx
@@ -38,24 +38,10 @@ const UserInfo = () => {
   const token = authService.getAccessToken();
 
   useEffect(() => {
-    // async function fetchUser() {
-    //   try {
-    //     if (token && id) {
-    //       const result = await userService.getById(token, id);
-    //       setUser(result);
-    //       setLoading(false);
-    //     }
-    //   } catch (err) {
-    //     console.log("Error getting user", err);
-    //     setError("Error fetching data");
-    //     setLoading(false);
-    //   }
-    // }
     if (token) {
       if (!user && id) {
         setError("");
         setLoading(true);
-        // fetchUser();
         userService
           .getById(token, id)
           .then((result) => {
